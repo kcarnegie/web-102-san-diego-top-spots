53337a6f51e63d8369c043d06bb28fba97bad4d5
kcarnegie <kevincarnegie@yahoo.com>
2022-06-06 13:46:27 -0700

    Links to San Diego's Top Spots

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
-// write your code here
+ $(document).ready(function() {
+    $.getJSON("./data.json", function(data) {
+        var topSpotData = '';
+        $.each(data, function(key, value) {
+            topSpotData += '<tr>';
+            topSpotData += '<td>' + value.name + '</td>';
+            topSpotData += '<td>' + value.description + '</td>';
+            topSpotData += "<td><a target='_blank' href='https://www.google.com/maps?q=" + value.location + "' >Link</a></td>";
+            topSpotData += '</tr>';
+        });
+        $('#myTableHeader').append(topSpotData);
+    });
+ });
 
-});
